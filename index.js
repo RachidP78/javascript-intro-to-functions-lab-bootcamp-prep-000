50f696faf09a29c8ab62f7083572a23df6b1d61d
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-19 13:31:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
   console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
   console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = string
+  if (uppercase.toUpperCase(string) === uppercase) {
+    return "I can't hear you"
+  }
+  var lowercase = string
+  else if (lowercase.toLowerCase(string) === lowercase) {
+    return "YES INDEED"
+  }
+  else {
+    return "I love you, too"
+  }
 }
